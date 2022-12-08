9e128b5aa35ecd7b749025f7ed1b1eed06062eff
A.Klein <allenkleined@gmail.com>
2022-12-08 14:16:22 -0800

    added typedef,resolvers on frontend, added mutations,queries on frontend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { Book, User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+module.exports=resolvers;
